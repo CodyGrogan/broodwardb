ffbb267d39b61b2b592920a0c4808a49ca509e6d
Cody Grogan <cod.grogan@gmail.com>
2022-05-03 20:41:07 -0400

    basic setup for express and react app

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('*', function(req, res) {
+
+  console.log("received *");
+  
+  let pathstring = (path.join(__dirname, '../client/build/index.html' ));
+  console.log(pathstring);
+  res.sendFile(pathstring);
 });
 
 module.exports = router;
