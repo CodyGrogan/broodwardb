53bab6f5fdc55c3eb87ae989d36e38dc91a913e8
Cody Grogan <cod.grogan@gmail.com>
2022-05-08 11:58:52 -0400

    basic page layout, router

--- client/src/components/PlayersPage.tsx
+++ client/src/components/PlayersPage.tsx
@@ -1,8 +1,14 @@
+import Navbar from "./Navbar";
 
 function PlayersPage(){
     
     return(
         <div>
+
+    <Navbar/>
+    <br/>            
+    <br/>
+
             this is the players page
         </div>
     )
