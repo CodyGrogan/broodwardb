318cdd1b8c44f3b81cdcf2976f5c20e48ee20d90
Cody Grogan <cod.grogan@gmail.com>
2022-06-03 17:19:09 -0400

    link to other player pages on game history table items

--- client/src/components/PlayersTableItem.tsx
+++ client/src/components/PlayersTableItem.tsx
@@ -2,12 +2,13 @@ import { Link } from "react-router-dom";
 
 
 function PlayersTableItem(props: any){
-    let linkstring = `/player/${props.id}`;
+    let linkstring = `/player/${props.name}`;
     return(
         <tr>
                         <td>{props.tablenum}</td>
                         <td><Link to={linkstring}>{props.name}</Link></td>
                         <td>{props.scrace}</td>
+                        <td>{props.elo}</td>
 
         </tr>
     )
